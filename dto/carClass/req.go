5b79782c0d7cd32ccb08032d06d53eec704f746b
toleee <octaadsp.camp@gmail.com>
2023-12-15 17:00:51 +0700

    [Update] CarClass DTO

--- dto/carClass/req.go
+++ dto/carClass/req.go
@@ -5,5 +5,4 @@ type CarClassReq struct {
 	CarTypeID   uint32 `json:"car_type_id" form:"car_type_id" validate:"required"`
 	Golongan    string `json:"golongan" form:"golongan" validate:"required"`
 	Status      string `json:"status" form:"status"`
-	PriceListID uint32 `json:"price_list_id" form:"price_list_id" validate:"required"`
 }
