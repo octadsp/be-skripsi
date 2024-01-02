7a43e55148b99c0384411040b1515f1996f3c9da
tole <octaadsp.camp@gmail.com>
2024-01-02 21:44:23 +0700

    [Update] varchar Author

--- models/notification.go
+++ models/notification.go
@@ -5,7 +5,7 @@ import "time"
 type Notification struct {
 	ID        uint32    `json:"id" gorm:"primary_key:auto_increment"`
 	UserID    uint32    `json:"user_id"`
-	AuthorBy  string    `json:"author_by"`
+	AuthorBy  string    `json:"author_by" gorm:"type: varchar(100)"`
 	Title     string    `json:"title" gorm:"type: varchar(100)"`
 	Message   string    `json:"message" gorm:"type: varchar(150)"`
 	IsRead    bool      `json:"is_read"`
