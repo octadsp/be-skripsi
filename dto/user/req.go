57f506bcf9ca3873b4e456513b469a9badc49781
Kevin Jaya Susilo <kevin.jaya@meteor.id>
2024-06-05 22:01:29 +0700

    fix(dto): remove required on UpdateUserAddressRequest

--- dto/user/req.go
+++ dto/user/req.go
@@ -14,9 +14,9 @@ type NewUserAddressRequest struct {
 }
 
 type UpdateUserAddressRequest struct {
-	ProvinceID     string `json:"province_id" form:"province_id" validate:"required"`
-	RegencyID      string `json:"regency_id" form:"regency_id" validate:"required"`
-	DistrictID     string `json:"district_id" form:"district_id" validate:"required"`
-	AddressLine    string `json:"address_line" form:"address_line" validate:"required"`
+	ProvinceID     string `json:"province_id" form:"province_id"`
+	RegencyID      string `json:"regency_id" form:"regency_id"`
+	DistrictID     string `json:"district_id" form:"district_id"`
+	AddressLine    string `json:"address_line" form:"address_line"`
 	DefaultAddress bool   `json:"default_address" form:"default_address"`
 }
